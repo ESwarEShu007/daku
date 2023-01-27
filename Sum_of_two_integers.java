98c9513241fe5042bd9f23d562f75d8f7eb592a6
ESwarEShu007 <111848625+ESwarEShu007@users.noreply.github.com>
2023-01-27 13:51:40 +0530

    java program

---
+++ Sum_of_two_integers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Sum_of_two_integers
+{
+	public static void main(String[] args) {
+	    System.out.println("enter two numbers to get its sum..");
+	    Scanner es=new Scanner(System.in);
+	    int a=es.nextInt();
+	     Scanner sc=new Scanner(System.in);
+	    int b=sc.nextInt();
+	    int sum=a+b;
+		System.out.println("the sum of two numbers is "+sum);
+	}
+}
